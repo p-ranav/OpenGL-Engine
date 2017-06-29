f149e1edfa62816cd04114288a2e998200ff592d
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-06-29 19:12:30 -0400

    Cleanedup object interface class

--- Engine/Source/Rendering/object.h
+++ Engine/Source/Rendering/object.h
@@ -13,20 +13,52 @@
 
 namespace Rendering
 {
+	/**
+	 * Interface Class for Rendered Objects in our Engine
+	 */
 	class ObjectInterface
 	{
 	public:
+
+		/**
+		 * Abstract Base Class Destructor
+		 */
 		virtual ~ObjectInterface() = 0;
 
+		/**
+		 * Every renderable object has a Draw call
+		 */
 		virtual void Draw() = 0;
+
+		/**
+		 * Every renderable object has a Update call
+		 */
 		virtual void Update() = 0;
-		virtual void SetProgram(GLuint shaderName) = 0;
+
+		/**
+		 * Set a shader program for this object
+		 */
+		virtual void SetProgram(GLuint shader_name) = 0;
+
+		/**
+		 * Teardown this object
+		 */
 		virtual void Destroy() = 0;
 
+		/**
+		 * Get the vertex array object of this object
+		 * @return Identifier for the vao of this object
+		 */
 		virtual GLuint GetVao() const = 0;
+
+		/**
+		 * Get the vertex buffer objects of this object
+		 * @return vector of identifiers for the vbos of this object
+		 */
 		virtual const std::vector<GLuint>& GetVbos() const = 0;
 	};
 
+	// Inline Default Destructor Implementation
 	inline ObjectInterface::~ObjectInterface() {}
 
 }
