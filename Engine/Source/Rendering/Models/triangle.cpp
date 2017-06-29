3e01c7c6109864b7d29a8d888ed874557989cd58
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-06-29 19:30:31 -0400

    Cleaned up triangle class

--- Engine/Source/Rendering/Models/triangle.cpp
+++ Engine/Source/Rendering/Models/triangle.cpp
@@ -1,27 +1,19 @@
+// User-defined Headers
 #include "triangle.h"
 
-using namespace Rendering;
-using namespace Models;
-
-Triangle::Triangle()
+/**
+ * (1) Create the set of vertices
+ * (2) Create and bind vbo and vao
+ */
+void Rendering::Models::Triangle::Create()
 {
-}
-
-Triangle::~Triangle()
-{
-	//is going to be deleted in Models.cpp (inheritance)
-}
-
-void Triangle::Create()
-{
-	//this is just copy past from
-	// http://in2gpu.com/2014/12/19/create-triangle-opengl-part-iii-attributes/
 	GLuint vao;
 	GLuint vbo;
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+	// Vertices + per-vertex Colors for this triangle
 	std::vector<VertexFormat> vertices;
 	vertices.push_back(VertexFormat(glm::vec3(0.25, -0.25, 0.0),
 		glm::vec4(1, 0, 0, 1)));
@@ -30,30 +22,33 @@ void Triangle::Create()
 	vertices.push_back(VertexFormat(glm::vec3(0.25, 0.25, 0.0),
 		glm::vec4(0, 0, 1, 1)));
 
+	// Bind vbo and link vertices
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexFormat) * 3, &vertices[0], GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexFormat),
-		(void*)0);
+	// Attribute Pointer for vertex position (vec3)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexFormat), (void*)0);
 	glEnableVertexAttribArray(1);
-	// you can use offsetof to get the offset of an attribute
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(VertexFormat),
-		(void*)(offsetof(VertexFormat, VertexFormat::color)));
+	// Attribute Pointer for vertex color (vec4)
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(VertexFormat), (void*)(offsetof(VertexFormat, VertexFormat::color)));
 	glBindVertexArray(0);
 
-	//here we assign the values
+	// Save vao and vbo in model
 	this->vao = vao;
 	this->vbos.push_back(vbo);
-
 }
 
-void Triangle::Update()
-{
-	//for triangle there is nothing to update for now
-}
+// Lame triangle is lame - No updates here
+void Rendering::Models::Triangle::Update() {}
 
-void Triangle::Draw()
+/**
+ * Overrides Model's Draw method
+ * (1) Use Shader Program
+ * (2) Bind avo
+ * (3) Draw vertices
+ */
+void Rendering::Models::Triangle::Draw()
 {
 	glUseProgram(program);
 	glBindVertexArray(vao);
--- Engine/Source/Rendering/Models/triangle.h
+++ Engine/Source/Rendering/Models/triangle.h
@@ -7,14 +7,40 @@ namespace Rendering
 {
 	namespace Models
 	{
+		/**
+		 * Triangle Class - our most basic model
+		 */
 		class Triangle : public Model
 		{
 		public:
-			Triangle();
-			~Triangle();
 
+			/**
+			 * Triangle Default Constructor
+			 */
+			Triangle() {}
+
+			/**
+			* Triangle Default Destructor
+			*/
+			~Triangle() {}
+
+			/**
+			 * (1) Create the set of vertices
+			 * (2) Create and bind vbo and vao
+			 */
 			void Create();
+
+			/**
+			 * Overrides Model's Update method
+			 */
 			virtual void Update() override final;
+
+			/**
+			 * Overrides Model's Draw method
+			 * (1) Use Shader Program
+			 * (2) Bind avo
+			 * (3) Draw vertices
+			 */
 			virtual void Draw() override final;
 		};
 	}
