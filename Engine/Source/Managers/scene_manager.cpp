2614f6748225156c70c195a9e2f39bf97d2d664a
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-06-29 19:08:46 -0400

    Cleaned up and commented SceneManager class

--- Engine/Source/Managers/scene_manager.cpp
+++ Engine/Source/Managers/scene_manager.cpp
@@ -1,5 +1,7 @@
+// User-defined Headers
 #include "scene_manager.h"
 
+// SceneManager Default Constructor - Construct shader manager and model manager here
 Managers::SceneManager::SceneManager()
 {
 	glEnable(GL_DEPTH_TEST);
@@ -8,26 +10,36 @@ Managers::SceneManager::SceneManager()
 	model_manager_ = new ModelManager();
 }
 
+// Delete shader manager and model manager here
 Managers::SceneManager::~SceneManager()
 {
 	delete shader_manager_;
 	delete model_manager_;
 }
 
+/**
+ * Call model manager's Update() which updates every model in the
+ * model manager's internal map
+ */ 
 void Managers::SceneManager::NotifyBeginFrame() {
 	model_manager_->Update();
 }
 
+/**
+ * Clear OpenGL bits and call the model manager's Draw() which draws
+ * every model in the model manager's internal map
+ */
 void Managers::SceneManager::NotifyDisplayFrame()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
-
 	model_manager_->Draw();
 }
 
+// Nothing to do here (for now)
 void Managers::SceneManager::NotifyEndFrame()
 {}
 
+// Nothing to do here (for now)
 void Managers::SceneManager::NotifyReshape(int width, int height, int previous_width, int previous_height)
 {}
--- Engine/Source/Managers/scene_manager.h
+++ Engine/Source/Managers/scene_manager.h
@@ -20,10 +20,28 @@ namespace Managers
 		/* SceneManager Default Destructor */
 		~SceneManager();
 	
-		// Display Functions
+		/**
+		 * Calls every model's update
+		 */
 		virtual void NotifyBeginFrame();
+
+		/**
+		 * Sets up OpenGL bits and calls every model's draw
+		 */
 		virtual void NotifyDisplayFrame();
+
+		/**
+		* Post-draw operations on all models
+		*/
 		virtual void NotifyEndFrame();
+
+		/**
+		 * Calls every model's reshape handler
+		 * @param width New window width
+		 * @param height New window height
+		 * @param previous_width Previous window width
+		 * @param previous_height Previous window height
+		 */
 		virtual void NotifyReshape(int width, int height, int previous_width, int previous_height);
 
 	private:
@@ -31,7 +49,7 @@ namespace Managers
 		/* SceneManager contains a shader manager */
 		Managers::ShaderManager* shader_manager_;
 
-		/*  */
+		/* SceneManager also contains a model manager */
 		Managers::ModelManager* model_manager_;
 	};
 }
