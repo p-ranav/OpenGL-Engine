1eea621929c9cefa8fdc099dfd68831b3ffb7e0f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-06-29 19:02:35 -0400

    Cleaned up and commented ModelManager class

--- Engine/Source/Managers/model_manager.cpp
+++ Engine/Source/Managers/model_manager.cpp
@@ -1,59 +1,54 @@
+// User-defined Headers
 #include "model_manager.h"
 
-using namespace Managers;
-using namespace Rendering;
-
-ModelManager::ModelManager()
+// Construct some models to render
+Managers::ModelManager::ModelManager()
 {
 	//triangle game object
-	Models::Triangle* triangle = new Models::Triangle();
+	Rendering::Models::Triangle* triangle = new Rendering::Models::Triangle();
 	triangle->SetProgram(ShaderManager::GetShader("colorShader"));
 	triangle->Create();
 	model_map_["triangle"] = triangle;
 
 	// quad game object
-	Models::Quad* quad = new Models::Quad();
+	Rendering::Models::Quad* quad = new Rendering::Models::Quad();
 	quad->SetProgram(ShaderManager::GetShader("colorShader"));
 	quad->Create();
 	model_map_["quad"] = quad;
 }
 
-ModelManager::~ModelManager()
+// Delete models in the internal map and clear the map
+Managers::ModelManager::~ModelManager()
 {
-	//auto -it's a map iterator
 	for (auto model : model_map_)
-	{
 		delete model.second;
-	}
 	model_map_.clear();
 }
 
-void ModelManager::DeleteModel(const std::string& gameModelName)
+// Fetch model from internal map (by name) and call model.Destroy()
+void Managers::ModelManager::DeleteModel(const std::string& model_name)
 {
-	ObjectInterface* model = model_map_[gameModelName];
+	Rendering::ObjectInterface* model = model_map_[model_name];
 	model->Destroy();
-	model_map_.erase(gameModelName);
+	model_map_.erase(model_name);
 }
 
-const ObjectInterface& ModelManager::GetModel(const std::string& gameModelName) const
+// Fetch model from internal map (by name) and return a const reference to this model
+const Rendering::ObjectInterface& Managers::ModelManager::GetModel(const std::string& model_name) const
 {
-	return (*model_map_.at(gameModelName));
+	return (*model_map_.at(model_name));
 }
 
-void ModelManager::Update()
+// Call model.Update() for each model in the internal map
+void Managers::ModelManager::Update()
 {
-	//auto -it's a map iterator
 	for (auto model : model_map_)
-	{
 		model.second->Update();
-	}
 }
 
-void ModelManager::Draw()
+// Call model.Draw() for each model in the internal map
+void Managers::ModelManager::Draw()
 {
-	//auto -it's a map iterator
 	for (auto model : model_map_)
-	{
 		model.second->Draw();
-	}
 }
--- Engine/Source/Managers/model_manager.h
+++ Engine/Source/Managers/model_manager.h
@@ -5,21 +5,57 @@
 #include "../Rendering/Models/triangle.h"
 #include "../Rendering/Models/quad.h"
 
-using namespace Rendering;
 namespace Managers
 {
+	/**
+	 * ModelManager Class contains an internal map of Objects keyed by name
+	 */
 	class ModelManager
 	{
 	public:
+
+		/**
+		 * ModelManager Default Constructor
+		 */
 		ModelManager();
+
+		/**
+		* ModelManager Default Destructor
+		*/
 		~ModelManager();
 
+		/**
+		 * This method calls the Draw() methods of each Object in 
+		 * the manager's internal map
+		 */
 		void Draw();
+
+		/**
+		* This method calls the Update() methods of each Object in
+		* the manager's internal map
+		*/
 		void Update();
-		void DeleteModel(const std::string& gameModelName);
-		const ObjectInterface& GetModel(const std::string& gameModelName) const;
+
+		/**
+		* This method deletes a model in the internal map
+		* @param model_name Name of the model to delete
+		*/
+		void DeleteModel(const std::string& model_name);
+
+		/**
+		* This method fetches a model from the internal map
+		* @param model_name Name of the model to fetch
+		* @return Constant Reference to the model fetched from the internal map
+		*/
+		const Rendering::ObjectInterface& GetModel(const std::string& model_name) const;
 
 	private:
-		std::map<std::string, ObjectInterface*> model_map_;
+
+		/** 
+		 * Internal map of models 
+		 * Key: Model Name
+		 * Value: Model Object
+		 */
+		std::map<std::string, Rendering::ObjectInterface*> model_map_;
 	};
 }
