ce72769c85617753dec60546b5bbcb984c2bafcc
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-06-29 19:22:10 -0400

    Cleaned up model base class

--- Engine/Source/Rendering/Models/model.cpp
+++ Engine/Source/Rendering/Models/model.cpp
@@ -1,36 +1,37 @@
 // User-defined Headers
 #include "model.h"
 
+// Call Destroy and cleanup vao and vbos of the model
 Rendering::Models::Model::~Model()
 {
 	Destroy();
 }
 
-void Rendering::Models::Model::Draw()
-{
-	//this will be again overridden
-}
+// Draw method will be overridden
+void Rendering::Models::Model::Draw() {}
 
-void Rendering::Models::Model::Update()
-{
-	//this will be again overridden
-}
+// Update method will be overridden
+void Rendering::Models::Model::Update() {}
 
+// Set the shader program of this model
 void Rendering::Models::Model::SetProgram(GLuint program)
 {
 	this->program = program;
 }
 
+// Return model's vao
 GLuint Rendering::Models::Model::GetVao() const
 {
 	return vao;
 }
 
+// Return const reference to model's vbos vector
 const std::vector<GLuint>& Rendering::Models::Model::GetVbos() const
 {
 	return vbos;
 }
 
+// Delete vao and vbos
 void Rendering::Models::Model::Destroy()
 {
 	glDeleteVertexArrays(1, &vao);
--- Engine/Source/Rendering/Models/model.h
+++ Engine/Source/Rendering/Models/model.h
@@ -16,23 +16,65 @@ namespace Rendering
 {
 	namespace Models
 	{
+		/**
+		 * Concrete model class that future models will inherit from and override
+		 */
 		class Model : public ObjectInterface
 		{
 		public:
+
+			/**
+			 * Model Default Constructor
+			 */
 			Model() {}
+
+			/**
+			* Model Default Destructor
+			*/
 			virtual ~Model();
-			// methods from interface
+
+			/**
+			* Every model has a Draw method - This method will be overridden
+			*/
 			virtual void Draw() override;
+
+			/**
+			 * Every model has a Update method - This method will be overridden
+			 */
 			virtual void Update() override;
-			virtual void SetProgram(GLuint shaderName) override;
+
+			/**
+			 * Set the internal shader program of this model
+			 * @param program Shader program 
+			 */
+			virtual void SetProgram(GLuint program) override;
+
+			/**
+			 * Every model has a Destroy method - Cleanup vao and vbos
+			 */
 			virtual void Destroy() override;
 
+			/**
+			 * Get the vertex array object of this object
+			 * @return Identifier for the vao of this object
+			 */
 			virtual GLuint GetVao() const override;
+
+			/**
+			 * Get the vertex buffer objects of this object
+			 * @return vector of identifiers for the vbos of this object
+			 */
 			virtual const std::vector<GLuint>& GetVbos() const override;
 
 		protected:
+
+			/* Vertex Array Object of this model */
 			GLuint vao;
+
+			/* Shader Program for this model */
 			GLuint program;
+
+			/* Vertex Buffer Objects of this model */
 			std::vector<GLuint> vbos;
 		};
 	}
