0ef971921da008410310e34fd86cbbfc038737f7
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-06-29 19:37:17 -0400

    Cleaned up quad class

--- Engine/Source/Rendering/Models/quad.cpp
+++ Engine/Source/Rendering/Models/quad.cpp
@@ -1,53 +1,51 @@
+// User-defined Headers
 #include "quad.h"
-using namespace Rendering;
-using namespace Models;
 
-Quad::Quad()
-{}
-
-Quad::~Quad()
-{}
-
-void Quad::Create()
+/**
+* (1) Create the set of vertices
+* (2) Create and bind vbo and vao
+*/
+void Rendering::Models::Quad::Create()
 {
 	GLuint vao;
 	GLuint vbo;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+	// Vertices + per-vertex Colors for this quad
 	std::vector<VertexFormat> vertices;
-	vertices.push_back(VertexFormat(glm::vec3(-0.25, 0.5, 0.0),//pos
-		glm::vec4(1, 0, 0, 1)));   //color
-	vertices.push_back(VertexFormat(glm::vec3(-0.25, 0.75, 0.0),//pos
-		glm::vec4(0, 0, 0, 1)));   //color
-	vertices.push_back(VertexFormat(glm::vec3(0.25, 0.5, 0.0),  //pos
-		glm::vec4(0, 1, 0, 1)));   //color
-								   //4th vertex
+	vertices.push_back(VertexFormat(glm::vec3(-0.25, 0.5, 0.0),
+		glm::vec4(1, 0, 0, 1)));
+	vertices.push_back(VertexFormat(glm::vec3(-0.25, 0.75, 0.0),
+		glm::vec4(0, 0, 0, 1)));
+	vertices.push_back(VertexFormat(glm::vec3(0.25, 0.5, 0.0),
+		glm::vec4(0, 1, 0, 1)));
 	vertices.push_back(VertexFormat(glm::vec3(0.25, 0.75, 0.0),//pos
-		glm::vec4(0, 0, 1, 1)));   //color
-								   //nothing different from Triangle model
+		glm::vec4(0, 0, 1, 1)));
+
+	// Bind vbo and link vertices
 	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);            //here we have 4
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexFormat) * 4, &vertices[0], GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
-		sizeof(VertexFormat), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexFormat), (void*)0);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE,
-		sizeof(VertexFormat),
-		(void*)(offsetof(VertexFormat, VertexFormat::color)));
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(VertexFormat), (void*)(offsetof(VertexFormat, VertexFormat::color)));
 	glBindVertexArray(0);
 	this->vao = vao;
 	this->vbos.push_back(vbo);
-
 }
 
-void Quad::Update()
-{
-	//nothing here for the moment
-}
+// Lame triangle is lame - No updates here
+void Rendering::Models::Quad::Update() {}
 
-void Quad::Draw()
+/**
+* Overrides Model's Draw method
+* (1) Use Shader Program
+* (2) Bind avo
+* (3) Draw vertices
+*/
+void Rendering::Models::Quad::Draw()
 {
 	glUseProgram(program);
 	glBindVertexArray(vao);
--- Engine/Source/Rendering/Models/quad.h
+++ Engine/Source/Rendering/Models/quad.h
@@ -7,14 +7,41 @@ namespace Rendering
 {
 	namespace Models
 	{
+
+		/**
+		* Quad Class
+		*/
 		class Quad : public Models::Model
 		{
 		public:
-			Quad();
-			~Quad();
 
+			/**
+			 * Quad Default Constructor
+			 */
+			Quad() {}
+
+			/**
+			* Quad Default Destructor
+			*/
+			~Quad() {}
+
+			/**
+			* (1) Create the set of vertices
+			* (2) Create and bind vbo and vao
+			*/
 			void Create();
+
+			/**
+			* Overrides Model's Update method
+			*/
 			virtual void Draw() override final;
+
+			/**
+			* Overrides Model's Draw method
+			* (1) Use Shader Program
+			* (2) Bind avo
+			* (3) Draw vertices
+			*/
 			virtual void Update() override final;
 		};
 	}
