c1317ccb7340699bdf07e9d3ac78e722f883cee9
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-06-30 22:07:57 -0400

    Added rotating cube project. This is the first 3d rendering test caser

---
+++ Models.RotatingCube/main.cpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+// User-defined Headers
+#include "Managers\engine_manager.h"
+#include "Rendering\Models\cube.h"
+
+int main(int argc, char **argv) 
+{
+	// Initialize the Engine Manager
+	Managers::EngineManager* engine = new Managers::EngineManager();
+	engine->Init();
+
+	// Setup Cube's Vertex and Fragment Shaders
+	engine->GetShaderManager()->CreateProgram("cubeShader", "Shaders\\cube_vertex_shader.glsl", "Shaders\\cube_fragment_shader.glsl");
+
+	// Create the Cube and set its shader program
+	Rendering::Models::Cube* cube = new Rendering::Models::Cube();
+	cube->SetProgram(engine->GetShaderManager()->GetShader("cubeShader"));
+	cube->Create();
+
+	// Add cube to Engine's Model Manager
+	engine->GetModelManager()->AddModel3D("cube", cube);
+
+	// Run the Engine
+	engine->Run();
+
+	// Delete the Engine Object when rendering loop is finished
+	delete engine;
+	return 0;
+}
